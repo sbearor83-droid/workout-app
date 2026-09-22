e8ed2289b71f95564a9cf9b4292064406f3bb6ae
sbearor83-droid <sbearor83@gmail.com>
2026-09-22 10:21:37 -0500

    Initial commit: Rack Log Pro - 30-minute garage lifting PWA

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = 'racklog-pro-v2';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.map((k) => (k !== CACHE_NAME ? caches.delete(k) : null))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((cached) => cached || fetch(e.request).catch(() => caches.match('./index.html')))
+  );
+});
